d3c4d89b0713d5d6d23a8f0359492a6f1ea73dc8
Falko <f.thomale@gmail.com>
2020-02-20 13:15:04 +0100

    implemented card dealing actions

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,12 @@
 module Main exposing (..)
 
+import Array exposing (Array)
+import Array.Extra
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Maybe.Extra
 import Random exposing (Seed)
 
 
@@ -31,9 +34,8 @@ type alias Model =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model <| Start <| StartInfo [ "Player 1" ]
-    , Cmd.none
-    )
+    --( Model <| Start <| StartInfo [ "Player 1" ], Cmd.none )
+    ( Model End, Random.generate (GameStarted <| StartInfo [ "Player 1", "Player 2", "Player 3" ]) Random.independentSeed )
 
 
 type GameState
@@ -48,19 +50,27 @@ type alias StartInfo =
 
 
 type alias GameInfo =
-    { players : List Player
-    , round : Int
+    { players : Array Player
+    , stage : Int
     , playerInTurn : Int
-    , middleDeck : List Card
-    , middleDeckSum : Int
+    , cardAction : CardAction
+    , cardToSharedDeck : Maybe Card
+    , cardToRoute : Maybe Card
+    , sharedDeck : Array Card
+    , sharedDeckSum : Int
     , randomnessSeed : Seed
     }
 
 
+type CardAction
+    = NoAction
+    | HandCardSelected Int
+
+
 type alias Player =
     { name : String
-    , hand : List Card
-    , route : List Card
+    , hand : Array Card
+    , route : Array Card
     }
 
 
@@ -80,9 +90,14 @@ type Msg
     = NameChanged Int String
     | RemovePlayer Int
     | AddPlayer
-    | StartGame
-    | GameStarted Seed
+    | StartGame StartInfo
+    | GameStarted StartInfo Seed
     | HandCardClicked Int
+    | AddToSharedDeckClicked Int
+    | TakeSharedCardBackClicked
+    | AddToRouteClicked Int
+    | TakeRouteCardBackClicked
+    | EndTurn
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -113,39 +128,123 @@ update msg model =
         RemovePlayer index ->
             ( updateStartInfo (\startInfo -> StartInfo (List.take index startInfo.players ++ List.drop (index + 1) startInfo.players)) model, Cmd.none )
 
-        StartGame ->
-            ( model, Random.generate GameStarted Random.independentSeed )
+        StartGame startInfo ->
+            ( model, Random.generate (GameStarted startInfo) Random.independentSeed )
 
-        GameStarted seed ->
-            ( Model <| Play <| initGame model seed, Cmd.none )
+        GameStarted startInfo seed ->
+            ( Model <| Play <| initGame startInfo seed, Cmd.none )
 
-        HandCardClicked int ->
-            ( updateGameInfo (\gameInfo -> gameInfo) model, Cmd.none )
+        HandCardClicked index ->
+            ( updateGameInfo (\gameInfo -> { gameInfo | cardAction = HandCardSelected index }) model, Cmd.none )
+
+        AddToRouteClicked selectedHandCardIndex ->
+            ( updateGameInfo
+                (\gameInfo ->
+                    { gameInfo
+                        | cardAction = NoAction
+                        , players = removeCardFromPlayersHand gameInfo.playerInTurn selectedHandCardIndex gameInfo.players
+                        , cardToRoute = getCardFromPlayersHand gameInfo.playerInTurn selectedHandCardIndex gameInfo.players
+                    }
+                )
+                model
+            , Cmd.none
+            )
+
+        TakeRouteCardBackClicked ->
+            ( updateGameInfo
+                (\gameInfo ->
+                    { gameInfo
+                        | cardAction = NoAction
+                        , players = addCardToPlayersHand gameInfo.playerInTurn gameInfo.cardToRoute gameInfo.players
+                        , cardToRoute = Nothing
+                    }
+                )
+                model
+            , Cmd.none
+            )
+
+        AddToSharedDeckClicked selectedHandCardIndex ->
+            ( updateGameInfo
+                (\gameInfo ->
+                    { gameInfo
+                        | cardAction = NoAction
+                        , players = removeCardFromPlayersHand gameInfo.playerInTurn selectedHandCardIndex gameInfo.players
+                        , cardToSharedDeck = getCardFromPlayersHand gameInfo.playerInTurn selectedHandCardIndex gameInfo.players
+                    }
+                )
+                model
+            , Cmd.none
+            )
+
+        TakeSharedCardBackClicked ->
+            ( updateGameInfo
+                (\gameInfo ->
+                    { gameInfo
+                        | cardAction = NoAction
+                        , players = addCardToPlayersHand gameInfo.playerInTurn gameInfo.cardToSharedDeck gameInfo.players
+                        , cardToSharedDeck = Nothing
+                    }
+                )
+                model
+            , Cmd.none
+            )
+
+        EndTurn ->
+            ( model, Cmd.none )
+
+
+addCardToPlayersHand : Int -> Maybe Card -> Array Player -> Array Player
+addCardToPlayersHand playerIndex card players =
+    case card of
+        Just card2 ->
+            Array.indexedMap
+                (\playerIndex2 player ->
+                    if playerIndex == playerIndex2 then
+                        { player | hand = Array.push card2 player.hand }
+
+                    else
+                        player
+                )
+                players
+
+        Nothing ->
+            players
+
+
+getCardFromPlayersHand : Int -> Int -> Array Player -> Maybe Card
+getCardFromPlayersHand playerIndex cardIndex players =
+    let
+        currentPlayer =
+            Array.get playerIndex players
+    in
+    Maybe.Extra.join <| Maybe.map (\player -> Array.get cardIndex player.hand) currentPlayer
+
+
+removeCardFromPlayersHand : Int -> Int -> Array Player -> Array Player
+removeCardFromPlayersHand playerIndex cardIndex players =
+    case Array.get playerIndex players of
+        Just a ->
+            Array.set playerIndex { a | hand = Array.Extra.removeAt cardIndex a.hand } players
+
+        Nothing ->
+            players
 
 
 cardGenerator : Random.Generator Card
 cardGenerator =
-    Random.uniform (Speed 0)
+    Random.uniform (Speed 10)
         [ Minus50
         , Tankstelle
-        , Nachziehkarte 0
+        , Nachziehkarte 1
         , Abwerfkarte
         ]
 
 
-initGame : Model -> Seed -> GameInfo
-initGame model seed =
+initGame : StartInfo -> Seed -> GameInfo
+initGame startInfo seed =
     let
         numCards =
             10
-
-        startInfo =
-            case model.gameState of
-                Start startInfo2 ->
-                    startInfo2
-
-                _ ->
-                    StartInfo []
 
         createDrawDeck =
             Random.list (numCards * List.length startInfo.players) cardGenerator
@@ -154,12 +253,12 @@ initGame model seed =
             Random.step createDrawDeck seed
 
         takeCards index deck =
-            List.take 10 <| List.drop (10 * index) deck
+            Array.fromList <| List.take numCards <| List.drop (numCards * index) deck
 
         players =
-            List.indexedMap (\index name -> Player name (takeCards index drawDeck) []) startInfo.players
+            Array.fromList <| List.indexedMap (\index name -> Player name (takeCards index drawDeck) Array.empty) startInfo.players
     in
-    GameInfo players 0 0 [] 0 newSeed
+    GameInfo players 0 0 NoAction Nothing Nothing Array.empty 0 newSeed
 
 
 updateStartInfo : (StartInfo -> StartInfo) -> Model -> Model
@@ -187,7 +286,7 @@ updateGameInfo updateFunction model =
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions _ =
     Sub.none
 
 
@@ -222,7 +321,7 @@ viewStartInfo startInfo =
     div []
         [ div [] <| List.indexedMap mapPlayersToInput startInfo.players
         , button [ onClick AddPlayer ] [ text "Add Player" ]
-        , button [ onClick StartGame ] [ text "Start Game" ]
+        , button [ onClick <| StartGame startInfo ] [ text "Start Game" ]
         ]
 
 
@@ -230,30 +329,118 @@ viewGame : GameInfo -> Html Msg
 viewGame gameInfo =
     let
         viewCards mf cards =
-            div [] <| List.indexedMap mf cards
+            div [] <| Array.toList <| Array.indexedMap mf cards
 
-        viewPlayer index player =
+        showCardBack attributes =
+            \_ _ -> button attributes [ text "X" ]
+
+        viewPlayer playerIndex player =
             let
-                showText =
-                    \_ card -> text <| Debug.toString card
+                showCardFront =
+                    \cardIndex card ->
+                        let
+                            colorAttributes =
+                                case gameInfo.cardAction of
+                                    HandCardSelected selectedCardIndex ->
+                                        if selectedCardIndex == cardIndex then
+                                            [ style "background-color" "#73D216" ]
 
-                showButton =
-                    \cardIndex card -> button [ onClick (HandCardClicked cardIndex) ] [ text <| Debug.toString card ]
+                                        else
+                                            []
+
+                                    _ ->
+                                        []
+                        in
+                        button
+                            ([ onClick (HandCardClicked cardIndex) ] ++ colorAttributes)
+                            [ text <| Debug.toString card ]
+
+                playerRouteCard =
+                    gameInfo.cardToRoute
+                        |> Maybe.map
+                            (\card ->
+                                div []
+                                    [ button [ disabled True ] [ text <| Debug.toString card ]
+                                    , button [ onClick TakeRouteCardBackClicked ] [ text "Take Card Back" ]
+                                    ]
+                            )
+                        |> Maybe.withDefault (text "")
             in
             div []
                 [ text <| player.name ++ ": "
                 , div []
-                    [ viewCards showButton player.hand
-                    , br [] []
-                    , viewCards showText player.route
-                    , br [] []
+                    [ viewCards
+                        (if playerIndex == gameInfo.playerInTurn then
+                            showCardFront
+
+                         else
+                            showCardBack [ disabled True ]
+                        )
+                        player.hand
+                    , viewCards (showCardBack [ disabled True ]) player.route
+                    , if playerIndex == gameInfo.playerInTurn then
+                        div []
+                            [ playerRouteCard
+                            , case gameInfo.cardAction of
+                                HandCardSelected selectedHandCardIndex ->
+                                    case gameInfo.cardToRoute of
+                                        Nothing ->
+                                            button [ onClick <| AddToRouteClicked selectedHandCardIndex ] [ text "Add To Route" ]
+
+                                        _ ->
+                                            text ""
+
+                                _ ->
+                                    text ""
+                            ]
+
+                      else
+                        text ""
+                    , div [] [ br [] [] ]
                     ]
                 ]
 
-        viewStats =
-            div [] [ text <| "Sum: " ++ String.fromInt gameInfo.middleDeckSum ]
+        viewShared =
+            let
+                playerSharedCard =
+                    gameInfo.cardToSharedDeck
+                        |> Maybe.map
+                            (\card ->
+                                div []
+                                    [ button [ disabled True ] [ text <| Debug.toString card ]
+                                    , button [ onClick TakeSharedCardBackClicked ] [ text "Take Card Back" ]
+                                    ]
+                            )
+                        |> Maybe.withDefault (text "")
+            in
+            div []
+                [ text "Shared Deck:"
+                , br [] []
+                , text <| "Sum: " ++ String.fromInt gameInfo.sharedDeckSum
+                , div []
+                    [ viewCards (showCardBack [ disabled True ]) gameInfo.sharedDeck
+                    , playerSharedCard
+                    , case gameInfo.cardAction of
+                        HandCardSelected selectedHandCardIndex ->
+                            case gameInfo.cardToSharedDeck of
+                                Nothing ->
+                                    button [ onClick <| AddToSharedDeckClicked selectedHandCardIndex ] [ text "Add To Shared Deck" ]
+
+                                _ ->
+                                    text ""
+
+                        _ ->
+                            text ""
+                    ]
+                ]
     in
     div []
-        [ div [] <| List.indexedMap viewPlayer gameInfo.players
-        , viewStats
+        [ div [] <| Array.toList <| Array.indexedMap viewPlayer gameInfo.players
+        , viewShared
+        , div [] [ br [] [] ]
+        , button
+            [ disabled (Maybe.Extra.isNothing gameInfo.cardToRoute || Maybe.Extra.isNothing gameInfo.cardToSharedDeck)
+            , onClick EndTurn
+            ]
+            [ text "End Turn" ]
         ]
